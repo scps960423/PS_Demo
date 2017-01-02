90a185b518252ed8bddbb1c48424803d3db77c6a
林韻如 <scps960423@gmail.com>
2017-01-02 16:21:44 +0800

    優化pag2's jquer & 增加按鈕回饋

--- js/pag2.js
+++ js/pag2.js
@@ -1,33 +1,11 @@
 $(document).ready(function() {
-    $(".p2_1").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 0) $(".p2_main:eq(" + i + ")").slideUp("slow");
-        $(".p2_main:eq(0)").slideToggle("slow");
-    });
-    $(".p2_2").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 1) $(".p2_main:eq(" + i + ")").slideUp("slow");
-
-        $(".p2_main:eq(1)").slideToggle("slow");
-    });
-    $(".p2_3").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 2) $(".p2_main:eq(" + i + ")").slideUp("slow");
-        $(".p2_main:eq(2)").slideToggle("slow");
-    });
-    $(".p2_4").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 3) $(".p2_main:eq(" + i + ")").slideUp("slow");
-        $(".p2_main:eq(3)").slideToggle("slow");
-    });
-    $(".p2_5").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 4) $(".p2_main:eq(" + i + ")").slideUp("slow");
-        $(".p2_main:eq(4)").slideToggle("slow");
-    });
-    $(".p2_6").click(function() {
-        for (var i = 0; i <= 5; i++)
-            if (i != 5) $(".p2_main:eq(" + i + ")").slideUp("slow");
-        $(".p2_main:eq(5)").slideToggle("slow");
-    });
+    for (var j = 0; j < 6; j++) {
+        $(".p2_but:eq(" + j + ")").click({
+            id: j
+        }, function(e) {
+            for (var i = 0; i <= 5; i++)
+                if (i != e.data.id) $(".p2_main:eq(" + i + ")").slideUp("slow");
+            $(".p2_main:eq(" + e.data.id + ")").slideToggle("slow");
+        });
+    }
 });
