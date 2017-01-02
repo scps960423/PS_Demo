e3eadd3165e8eac92f90d0298f0e48b8722010be
林韻如 <scps960423@gmail.com>
2017-01-02 14:51:53 +0800

    新增第二頁內文

---
+++ js/pag2.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $(".p2_1").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(0)").slideToggle("slow");
+    });
+    $(".p2_2").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(1)").slideToggle("slow");
+    });
+    $(".p2_3").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(2)").slideToggle("slow");
+    });
+    $(".p2_4").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(3)").slideToggle("slow");
+    });
+    $(".p2_5").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(4)").slideToggle("slow");
+    });
+    $(".p2_6").click(function() {
+        $(".p2_main").slideUp("slow");
+        $(".p2_main:eq(5)").slideToggle("slow");
+    });
+});
