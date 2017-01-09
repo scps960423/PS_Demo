74b138a1d035df2eec498eac0e6eb73a9df4cbbc
林韻如 <scps960423@gmail.com>
2017-01-09 14:56:25 +0800

    完成第四頁面

--- js/pag2.js
+++ js/pag2.js
@@ -18,15 +18,31 @@ $(document).ready(function() {
             $(".p3_rwdMain:eq(" + e.data.id + ")").slideToggle("slow");
         });
     }
+    //
+    // $(".p4_c:eq(0)").click(function() {
+    //
+    //     // if (i != e.data.id) $(".p3_rwdMain:eq(" + i + ")").slideUp("slow");
+    //     $(".p4_main_1").slideToggle("slow");
+    //
+    // });
+    for (var j = 0; j < 6; j++) {
+        $(".p4_c:eq(" + j + ")").click({
+            id: j
+        }, function(e) {
+            // for (var i = 0; i <= 6; i++)
+            //     if (i != e.data.id) $(".p4_main_1:eq(" + i + ")").slideUp("slow");
+            $(".p4_main_1:eq(" + e.data.id + ")").slideToggle("slow");
+        });
+    }
 
-    $(".p4_c:eq(0)").click(function() {
-
-        // if (i != e.data.id) $(".p3_rwdMain:eq(" + i + ")").slideUp("slow");
-        $(".p4_main_1").slideToggle("slow");
-
-    });
-
-
-
+    for (var j = 0; j < 6; j++) {
+        $(".p4_but:eq(" + j + ")").click({
+            id: j
+        }, function(e) {
+            for (var i = 0; i <= 5; i++)
+                if (i != e.data.id) $(".p4_main:eq(" + i + ")").slideUp("slow");
+            $(".p4_main:eq(" + e.data.id + ")").slideToggle("slow");
+        });
+    }
 
 });
